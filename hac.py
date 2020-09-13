9ed9cc68e1b9154edda3f38905712aa149112d8d
XuliGan4eg2006 <62839426+XuliGan4eg2006@users.noreply.github.com>
2020-09-13 17:20:23 +0300

    Add files via upload

---
+++ hac.py
@@ -0,0 +1,27 @@
+from telethon import TelegramClient
+import time
+
+
+api_id = int(input("Enter api_id from my.telegram.org: "))
+api_hash = input("Enter api_hash from my.telegram.org: ")
+client = TelegramClient('anon', api_id, api_hash)
+
+chid = input("Enter chat id/@ to hack! : ")
+
+async def main():
+	megas = await client.send_message(chid, '`Hacking...`')
+	time.sleep(1)
+	await client.edit_message(megas, '`Hacking... 1% \n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒`')
+	time.sleep(2)
+	await client.edit_message(megas, '`Hacking... 5% \n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒`')
+	time.sleep(2)
+	await client.edit_message(megas, '`Hacking... 30% \n████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒`')
+	time.sleep(2)
+	await client.edit_message(megas, '`Hacking... 70% \n██████████████████████▒▒`')
+	time.sleep(2)
+	await client.edit_message(megas, '`Hacking... 100% \n████████████████████████`')
+	time.sleep(1)
+	await client.edit_message(megas, '`Hacked!`')
+
+with client:
+    client.loop.run_until_complete(main())
